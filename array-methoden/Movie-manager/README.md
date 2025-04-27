a060d03d5e0a083fae5ee116e5bc418fb145ef9c
Nam Doan <namdoan@MacBook-Pro-von-Nam.local>
2025-04-27 16:56:02 +0200

    feature: Mini-Projekt Movie Manager hinzugefügt

---
+++ array-methoden/Movie-manager/README.md
@@ -0,0 +1,18 @@
+# 🎬 Movie Manager
+
+Ein kleines JavaScript-Projekt zum verwalten von Filmen.
+
+## Funktionen
+
+- Filme zur Liste hinzufügen
+- Alle Filme anzeigen
+- Filme nach Genre filtern
+- Durchschnittliche Bewertung berechnen
+- Prüfen, ob es Top-Filme gibt
+- Filme alphabetisch nach Titel sortieren
+
+## Verwendete Konzepte
+
+- Arrays und Objekte
+- Array-Methoden: `map()`, `filter()`, `reduce()`, `some()`, `sort()`
+- Funktionen und saubere Modularisierung
---
+++ array-methoden/Movie-manager/movie-manager.js
@@ -0,0 +1,70 @@
+let filme = [];
+
+// Filme hinzufügen (Titel, Genre, Bewertung von 1 -10)
+function filmHinzufuegen(filmArray, titel, genre, bewertung) {
+  const neueFilm = {
+    titel: titel,
+    genre: genre,
+    bewertung: bewertung,
+  };
+
+  filmArray.push(neueFilm);
+}
+
+// Alle Filme anzeigen
+function zeigeAlleFilme(filmArray) {
+  return filmArray.map((film) => ({
+    titel: film.titel,
+    genre: film.genre,
+    bewertung: film.bewertung,
+  }));
+}
+
+// Nur bestimmte Genres filtern (z.B.: nur "Action"-Filme)
+function filterFilmeNachGenre(filmArray, genre) {
+  return filmArray.filter((film) => film.genre === genre);
+}
+
+// Durchschnittsbewertung aller Filme berechnen
+function berechneDurchschnittBewertung(filmArray) {
+  const gesamtBewertung = filmArray.reduce(
+    (summe, film) => summe + film.bewertung,
+    0
+  );
+  return gesamtBewertung / filmArray.length;
+}
+
+// Prüfen, ob es min. einen Film mit Bewertung über mindestBewertung gibt
+function hatTopFilm(filmArray, mindestBewertung) {
+  return filmArray.some((film) => film.bewertung > mindestBewertung);
+}
+
+// Filme alphabetisch sortieren
+function sortiereFilmeNachTitel(filmArray) {
+  return filmArray.map((film) => film.titel).sort();
+}
+
+// Filme hinzufügen
+filmHinzufuegen(filme, "Inception", "Sci-Fi", 9);
+filmHinzufuegen(filme, "Avengers", "Action", 8);
+filmHinzufuegen(filme, "Titanic", "Drama", 7);
+
+// Alle Filme zeigen
+console.log("Alle Filme:");
+console.log(zeigeAlleFilme(filme));
+
+// Filme nach Genre filtern
+console.log("Action Filme:");
+console.log(filterFilmeNachGenre(filme, "Action"));
+
+// durchschnittliche Bewertung
+console.log("Durchschnittliche Bewertung:");
+console.log(berechneDurchschnittBewertung(filme));
+
+// Prüfen nach Top Filme
+console.log("Ist ein Top Film dabei:");
+console.log(hatTopFilm(filme, 8));
+
+// Filme nach Alphabet sortieren
+console.log("Deine Filme:");
+console.log(sortiereFilmeNachTitel(filme));
